da090bef40d9ab6c8bd9b38cd038775d702e19d1
marwenez <labidimarwen6@gmail.com>
2022-01-26 22:19:58 +0100

    #js : i create all the functionality but there is a bug #bug #fix i should fix it tommoroe in [dom.js] line 152

--- src/js/dom.js
+++ src/js/dom.js
@@ -3,7 +3,16 @@ const sectionOne = document.querySelector('.section-1');
 const sectionTwo = document.querySelector('.section-2');
 const inputOne = document.querySelector('.search-1');
 const inputTwo = document.querySelector('.search-2');
+const dataListOne = document.querySelector('#listOne');
 const API_KEY = `33ea77d3f16fb2b8edff2abc5b2e606a`;
+let title = null;
+let overview = null;
+let releaseDate = null;
+let rating = null;
+let urlImage = null;
+let cardOne = null;
+let notFound = null;
+const movieList = {}
 
 
 
@@ -83,8 +92,25 @@ const getImageURL = async (poster_path) => {
 	return imageURL
 }
 
+const addMovieToList = (movie, dataList) => {
+	if (movie.length <= 0) {
+		return
+	}
+	const length = movie.length;
+	for (let i = 0; i < length; i++) {
+		movieList[`movie` + i] = document.createElement('option');
+		movieList[`movie` + i].setAttribute('value', movie[i].original_title);
+		dataList.append(movieList[`movie` + i]);
+	}
+}
 
-
+const pickTheChosenOne = (list, chosenName) => {
+	list.map((movie) => {
+		if (movie.original_title === chosenName) {
+			return movie;
+		}
+	})
+}
 
 
 const note = createNote('Searches for a movie on both sides to get started!');
@@ -92,23 +118,74 @@ body.append(note);
 
 
 inputOne.addEventListener('keyup', (e) => {
+	if (e.target.value.replace(/\s+/g, '').length <= 0) {
+		if (cardOne) {
+
+			cardOne.remove()
+			cardOne = null
+		}
+		if (notFound) {
+			notFound.remove()
+			notFound = null
+		}
+		body.append(note);
+		return
+	}
+
 	note.remove();
-	getMoviesInfo(API_KEY, e.target.value, "en-US", "US").then((MoviesLists) => {
+	getMoviesInfo(API_KEY, e.target.value.replace(/\s+/g, ''), "en-US", "US").then((MoviesLists) => {
 		const moviesList = MoviesLists.results
+		if (moviesList.length === 0) {
+			if (cardOne) {
+				cardOne.remove()
+			}
+			if (notFound === null) {
+				notFound = createNote('No results found');
+				body.append(notFound);
+			}
+			return
+		}
 		const top5 = getMostPopularMoviesTop5(moviesList)
-		const MOVIE = top5[0]
+		// create the dataset for the input here 
+		addMovieToList(top5, dataListOne);
+		
+			let chosenOne = inputOne.value;  //FIXME: not a good smell
+
+		const MOVIE = pickTheChosenOne(top5, chosenOne);
+		if (!MOVIE) {
+			return
+		}
 		getImageURL(MOVIE.poster_path).then((URLimg) => {
-			const card1=createCard(MOVIE.original_title,MOVIE.overview,MOVIE.release_date,MOVIE.vote_average,URLimg);
-			sectionOne.append(card1);
-
+			title = MOVIE.original_title;
+			overview = MOVIE.overview;
+			releaseDate = MOVIE.release_date;
+			rating = MOVIE.vote_average;
+			urlImage = URLimg;
+			setTimeout(() => {
+				if (title && overview && releaseDate && rating && urlImage) {
+					if (cardOne !== null) {
+						cardOne.remove()
+					}
+					const card1 = createCard(title, overview, releaseDate, rating, urlImage);
+					sectionOne.append(card1);
+					cardOne = document.querySelector('.card-1');
+					if (notFound) {
+						notFound.remove()
+					}
+				}
+			}, 1000);
 		})
-
 	})
+
 
 })
 
 
-//FIXME error when input is empty
+
+
+
+
+
 //TODO show a list of the film in select item and when you click it then i added automaticly
 //TODO create the functionality of the seconde input
 //TODO show the winner movie by applying the green color to all component and the lose movie red color
